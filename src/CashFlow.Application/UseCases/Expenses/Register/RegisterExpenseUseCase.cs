5e16d718064423f2087f6a80cdd39d72570d3871
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-03-24 21:17:27 -0300

    Feat : Estrutura do Projeto:
    Controller (Recebe a Requisição): O Controller de Despesas recebe a requisição HTTP, obtém os dados do request e chama o serviço da camada Application.
    
    Application (Executa a Lógica de Negócio): A camada Application processa a lógica de criação da despesa e retorna a despesa criada após aplicar as regras de negócio.
    
    Communication (Interage com Sistemas Externos): A camada Communication gerencia a troca de dados com sistemas externos, fornecendo as informações necessárias para a criação da despesa.
    
    Fluxo: A requisição é recebida pelo Controller, que interage com Communication e passa os dados para Application, que cria e retorna a despesa.

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredJson Execute(RequestRegisterExpenseJson request)
+        {
+            // TO DO Validations
+
+
+            return new ResponseRegisteredJson();
+        }
+    }
+}
---
+++ src/CashFlow.Communication/Enums/PaymentTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Communication.Enums
+{
+    public enum PaymentTypes
+    {
+        Cash = 0,
+        CreditCard = 1,
+        DebitCard = 2,
+        EletronicTransfer = 3,
+    }
+}
