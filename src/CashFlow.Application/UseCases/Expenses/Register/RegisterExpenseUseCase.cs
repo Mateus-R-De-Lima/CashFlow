c4c3e712e86ee121b4b7c7bff112caf580c2306c
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-03-24 21:37:18 -0300

    Feat: Realizando validação de request para criação de despesa de forma simplificada
    Foi implementada uma validação básica para a criação de uma despesa, utilizando um simples if para verificar se todos os campos obrigatórios estão presentes no request.

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,7 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using System.Data;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
@@ -7,10 +9,39 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisteredJson Execute(RequestRegisterExpenseJson request)
         {
-            // TO DO Validations
-
+            Validate(request);
 
             return new ResponseRegisteredJson();
+        }
+
+        /// <summary>
+        /// Validar os dados de request
+        /// </summary>
+        /// <param name="request"></param>
+        private void Validate(RequestRegisterExpenseJson request)
+        {   
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("The title is required.");
+            }
+
+            if(request.Amount <= 0)
+            {
+                throw new ArgumentException("The Amount must be greater than zero.");
+            }
+
+            var result = DateTime.Compare(request.Date,DateTime.UtcNow);
+            if(result > 0)
+            {
+                throw new ArgumentException("Expenses cannot be for the future");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentTypes), request.PaymentType);
+            if (!paymentTypeIsValid)
+            {
+                throw new ArgumentException("Payment Type is not valid.");
+            }
         }
     }
 }
