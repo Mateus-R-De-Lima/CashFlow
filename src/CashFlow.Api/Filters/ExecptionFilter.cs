0c6f4ef537870c9e1a319f46d001bda125ce134f
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-09-02 23:34:24 -0300

    feat: implementando filtro global de exceptions com IExceptionFilter
    - Criada a classe `ExceptionFilter` implementando a interface `IExceptionFilter` do ASP.NET Core.
    
    - Esse filtro permite capturar globalmente exceções lançadas durante o processamento das requisições, eliminando a necessidade de blocos try/catch em cada controller ou endpoint.

---
+++ src/CashFlow.Api/Filters/ExecptionFilter.cs
@@ -0,0 +1,47 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters
+{
+    public class ExecptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is CashFlowException)
+            {
+                HandleProjectExecption(context);
+            }
+            
+            else
+            {
+                ThrowUnkowError(context);
+            }
+        }
+
+        private void HandleProjectExecption(ExceptionContext context)
+        {
+            if(context.Exception is ErrorOnValidationException)
+            {
+                var ex = (ErrorOnValidationException)context.Exception;
+
+                var errorResponse = new ResponseErrorJson(ex.ErrorMessages);
+
+                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Result = new ObjectResult(errorResponse);
+
+            }
+
+        }
+
+        private void ThrowUnkowError(ExceptionContext context)
+        {
+            var errorResponse = new ResponseErrorJson("Unknown Error");
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
+          
+        }
+    }
+}
