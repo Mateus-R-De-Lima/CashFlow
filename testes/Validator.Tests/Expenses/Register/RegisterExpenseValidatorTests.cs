dd141de712e8963481301ad70e877dd435afbd69
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-09-03 23:14:55 -0300

    test: Adicionado primeiro teste unitário para validação de request mockada
    Criado teste unitário básico.
    
    Simulada requisição com dados mockados para validar comportamento esperado do endpoint/método.
    
    Verificada a resposta para assegurar que a requisição foi processada corretamente.
    
    Estruturado cenário de teste com padrão AAA (Arrange, Act, Assert) para manter clareza e organização.

---
+++ testes/Validator.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace Validator.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Sucesso()
+        {
+
+            // Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = new RequestRegisterExpenseJson
+            {
+                Amount = 100,
+                Date = DateTime.Now.AddDays(-1),
+                Title = "Title",
+                Description = "Description",
+                PaymentType = PaymentTypes.EletronicTransfer
+            };
+
+            //Act
+
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
