11833ec56b01e98abac58f24753c762fb5221185
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-09-06 18:10:25 -0300

    feat: Entidade Expense foi criada para representar despesas
    Adicionada a classe Expense com os campos necessários para controle financeiro,
    como Id, Title, Description, Date,PaymentType e Amount .
    
    Essa entidade será usada nas operações de CRUD

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities
+{
+    public class Expense
+    {
+
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public PaymentTypes PaymentType { get; set; }
+
+        public decimal Amount { get; set; }
+
+    }
+}
