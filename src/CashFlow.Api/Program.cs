ffad9eea3c55ea8e45148fcf2614e3b7f643843f
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-09-16 20:45:29 -0300

    feat-Configurando Injeção de dependências para o repositório
    Criação da classe estática `DependencyInjectionExtension` contendo o método de extensão `AddInfrastructure`, que encapsula a configuração de injeções de dependência relacionadas à camada de infraestrutura.
    
    Atualmente, o método registra o serviço `IExpensesRepository` com a implementação `ExpensesRepository` como Scoped.
    
    Esse método é utilizado no Program.cs por meio da chamada:
    
        builder.Services.AddInfrastructure();
    
    A abordagem melhora a organização do código, separando as responsabilidades e mantendo o Program.cs mais limpo e modular.

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,5 +1,6 @@
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
+using CashFlow.Infrastructure;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure();
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExecptionFilter)));
 
