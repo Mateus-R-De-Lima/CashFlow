55d955542f67145d2c9dedd4eb562899d1693cc4
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-16 22:37:15 -0300

    feat - Configurando o documento PDF

--- src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
+++ src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Reports.PDF.Fonts;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Reports.PDF
@@ -21,7 +22,25 @@ namespace CashFlow.Application.UseCases.Reports.PDF
             if (expenses.Count == 0)
                 return [];
 
+            var document = CreateDocument(month);
+
             return [];
+
+        }
+
+
+        private Document CreateDocument(DateOnly month)
+        {
+            var document = new Document();
+            document.Info.Title = $"{ResourceReportGenerationMessages.EXPENSES_FOR} {month:Y}";
+            document.Info.Author = "Mateus Lima";
+
+            var style = document.Styles["Normal"];
+            style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+
+
+
+            return document;
 
         }
     }
