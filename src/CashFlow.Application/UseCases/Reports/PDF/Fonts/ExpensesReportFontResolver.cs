ae6da0fc3afc3ddb7ebf078999d7d745e41de6d0
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-16 22:27:47 -0300

    feat -  implementação de uma função para retornar um array de bytes de arquivos de fontes.

--- src/CashFlow.Application/UseCases/Reports/PDF/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Reports/PDF/Fonts/ExpensesReportFontResolver.cs
@@ -1,4 +1,5 @@
 ﻿using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace CashFlow.Application.UseCases.Reports.PDF.Fonts
 {
@@ -6,12 +7,30 @@ namespace CashFlow.Application.UseCases.Reports.PDF.Fonts
     {
         public byte[]? GetFont(string faceName)
         {
-            throw new NotImplementedException();
+            var stream = ReadFontFile(faceName) ?? ReadFontFile(FontHelper.DEFAULT_FONT);
+
+            var length = (int)stream!.Length;
+
+            var data = new byte[length];
+
+            stream.Read(buffer: data,
+                        offset: 0,
+                        count: length);
+
+            return data;
         }
 
         public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
-        {           
+        {
             return new FontResolverInfo(familyName);
+        }
+
+
+        private Stream? ReadFontFile(string faceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Reports.PDF.Fonts.{faceName}.ttf");
         }
     }
 }
