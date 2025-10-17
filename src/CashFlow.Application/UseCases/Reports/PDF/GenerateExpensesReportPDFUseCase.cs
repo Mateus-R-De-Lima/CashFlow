2f88c1baace98eed298df59c0ca1f3078e72abc3
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-16 22:12:21 -0300

    feat - Criando fonte resolver e instalando PDFsharp-MigraDoc

--- src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
+++ src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
@@ -1,17 +1,23 @@
-﻿using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.UseCases.Reports.PDF.Fonts;
+using CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Reports.PDF
 {
-    public class GenerateExpensesReportPDFUseCase(
-         IExpensesReadOnlyRepository repository
-        ) : IGenerateExpensesReportPDFUseCase
+    public class GenerateExpensesReportPDFUseCase : IGenerateExpensesReportPDFUseCase
     {
+        private readonly IExpensesReadOnlyRepository _repository;
+        public GenerateExpensesReportPDFUseCase(IExpensesReadOnlyRepository repository)
+        {
+            _repository = repository;
+            GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
+        }
 
         private const string CURRENCY_SYMBOL = "R$";
 
         public async Task<byte[]> Execute(DateOnly month)
         {
-            var expenses = await repository.FilterByMonth(month);
+            var expenses = await _repository.FilterByMonth(month);
             if (expenses.Count == 0)
                 return [];
 
