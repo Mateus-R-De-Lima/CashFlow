3fa34d7c6e1a9962c972c935ba36d33487fec45f
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-20 22:03:35 -0300

    refacor - Organizando o codigo.

--- src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
+++ src/CashFlow.Application/UseCases/Reports/PDF/GenerateExpensesReportPDFUseCase.cs
@@ -3,6 +3,7 @@ using CashFlow.Domain.Repositories.Expenses;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.Rendering;
 using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace CashFlow.Application.UseCases.Reports.PDF
 {
@@ -26,33 +27,10 @@ namespace CashFlow.Application.UseCases.Reports.PDF
             var document = CreateDocument(month);
 
             var page = CreatePage(document);
+            CreateHeaderWithProfilePhotoAndName(page);
 
-            var table = page.AddTable();
-            table.AddColumn("210");
-            table.AddColumn("400");
-            
-            var row = table.AddRow();
-
-            row.Cells[0].AddImage("C:\\Users\\Mateus\\Downloads\\foto.png");
-            
-
-            row.Cells[1].AddParagraph("Hey, Mateus Lima");
-            row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
-            row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
-
-            var paragraph = page.AddParagraph();
-
-            paragraph.Format.SpaceAfter = "40";
-            paragraph.Format.SpaceBefore = "40";
-
-            var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
-            paragraph.AddFormattedText(title, new Font { Size = 15,Name =FontHelper.RALEWAY_REGULAR});
-
-            paragraph.AddLineBreak(); // Quebra de Linha
-
-            var totalExpenses = expenses.Sum(expenses => expenses.Amount);
-
-            paragraph.AddFormattedText($"{CURRENCY_SYMBOL} {totalExpenses}", new Font { Size = 50, Name = FontHelper.WORKSANS_BLACK });
+            var totalExpenses = expenses.Sum(expense => expense.Amount);
+            CreateTotalSpentSection(page, month, totalExpenses);
 
             return RenderDocument(document);
 
@@ -88,7 +66,39 @@ namespace CashFlow.Application.UseCases.Reports.PDF
             return section;
         }
 
+        private void CreateHeaderWithProfilePhotoAndName(Section page)
+        {
+            var table = page.AddTable();
+            table.AddColumn("210");
+            table.AddColumn("300");
 
+            var row = table.AddRow();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var directoryName = Path.GetDirectoryName(assembly.Location);
+            var pathFile = Path.Combine(directoryName!, "Logo", "foto.png");
+
+            row.Cells[0].AddImage(pathFile);
+
+            row.Cells[1].AddParagraph("Hey, Mateus Lima");
+            row.Cells[1].Format.Font = new Font { Name = FontHelper.RALEWAY_BLACK, Size = 16 };
+            row.Cells[1].VerticalAlignment = MigraDoc.DocumentObjectModel.Tables.VerticalAlignment.Center;
+        }
+
+        private void CreateTotalSpentSection(Section page, DateOnly month, decimal totalExpenses)
+        {
+            var paragraph = page.AddParagraph();
+            paragraph.Format.SpaceBefore = "40";
+            paragraph.Format.SpaceAfter = "40";
+
+            var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
+
+            paragraph.AddFormattedText(title, new Font { Name = FontHelper.RALEWAY_REGULAR, Size = 15 });
+
+            paragraph.AddLineBreak();
+
+            paragraph.AddFormattedText($"{CURRENCY_SYMBOL} {totalExpenses}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
+        }
         private byte[] RenderDocument(Document document)
         {
             var renderer = new PdfDocumentRenderer{ Document = document,};
