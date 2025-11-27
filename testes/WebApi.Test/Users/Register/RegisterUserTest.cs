d59f00dd851f6fd83930447a0c2b9b8c6e1b6851
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-11-26 22:10:06 -0300

    feat - Validações de Erros de requisções com Nome Vazio.

--- testes/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ testes/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,6 +1,11 @@
-﻿using CommonTestUtilities.Request;
+﻿using Bogus.Bson;
+using CashFlow.Exception;
+using CommonTestUtilities.Request;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -37,5 +42,55 @@ namespace WebApi.Test.Users.Register
             Assert.Equal(request.Name, response.RootElement.GetProperty("name").GetString());
 
         }
+
+
+
+        [Fact]
+        public async Task Error_Empty_Name()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var language = CultureInfo.CurrentCulture.Name;
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(language));
+
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+            var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray().ToList();
+           
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.Single(errors);
+            Assert.Equal(ResourceErrorMessages.NAME_EMPTY, errors[0].GetString());
+
+        }
+
+
+        [Fact]
+        public async Task Error_Empty_Name_FR()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+            
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("fr"));
+
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+            var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray().ToList();
+
+            var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMPTY", new CultureInfo("fr"));
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+            Assert.Single(errors);
+            Assert.Equal(expectedMessage, errors[0].GetString());
+
+        }
+
+
     }
 }
