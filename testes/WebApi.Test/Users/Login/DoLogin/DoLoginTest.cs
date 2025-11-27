ec1b6468712b97cd4b9973d2e8b2b7e9057ef699
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-11-26 22:52:11 -0300

    feat- Testes de Login

---
+++ testes/WebApi.Test/Users/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,54 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Test.Users.Login.DoLogin
+{
+    public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private const string METHOD = "api/Login";
+        private readonly HttpClient _httpClient;
+
+        private readonly string _email;
+        private readonly string _name;
+        private readonly string _password;
+
+        public DoLoginTest(CustomWebApplicationFactory webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+
+            _email = webApplicationFactory.GetEmail();
+            _name = webApplicationFactory.GetName();
+            _password = webApplicationFactory.GetPassword();   
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestLoginJson
+            {
+                Email = _email,
+                Password = _password
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            var body = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(body);
+
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.NotEmpty(responseData.RootElement.GetProperty("token")
+                .GetString());
+
+            Assert.Equal(_name, responseData.RootElement.GetProperty("name").GetString());
+
+
+
+        }
+    }
+}
