9fa53578d166e773243aab35019bcc0465418248
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-12-01 22:14:40 -0300

    feat - Implementando autorização com permissão de administrador para geração de relatórios.

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Reports.Excel;
 using CashFlow.Application.UseCases.Reports.PDF;
+using CashFlow.Domain.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
@@ -8,7 +9,7 @@ namespace CashFlow.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    [Authorize(Roles = Roles.ADMIN)]
     public class ReportController : ControllerBase
     {
 
