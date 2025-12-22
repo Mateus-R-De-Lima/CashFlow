8b492e5b9c3d69ff4c922c3bb0db8e486d01a2fa
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-12-22 20:29:09 -0300

    feat : test de alteração de senha.

---
+++ testes/CommonTestUtilities/Request/RequestChangePasswordJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using CashFlow.Communication.Requests;
+using CashFlow.Domain.Entities;
+
+namespace CommonTestUtilities.Request
+{
+    public class RequestChangePasswordJsonBuilder 
+    {
+
+        public static RequestChangePasswordUserJson Build()
+        {
+            return new Faker<RequestChangePasswordUserJson>()
+                .RuleFor(user => user.Password , faker => faker.Internet.Password())
+                .RuleFor(user => user.NewPassword , faker => faker.Internet.Password(prefix:"!Aa1"));
+        }
+    }
+}
---
+++ testes/UseCase.Testes/User/ChangePassword/ChangePasswordUseCaseTest.cs
@@ -0,0 +1,69 @@
+﻿using CashFlow.Application.UseCases.User.ChangePassword;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Request;
+
+namespace UseCase.Tests.User.ChangePassword
+{
+    public class ChangePasswordUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            //Arrange
+            var user = UserBuilder.Build();
+            var request = RequestChangePasswordJsonBuilder.Build();
+
+            var useCase = CreateUseCase(user, request.Password);
+            //Act
+            await useCase.Execute(request);
+            //Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task Error_NewPassword_Empty()
+        {
+            var user = UserBuilder.Build();
+
+            var request = RequestChangePasswordJsonBuilder.Build();
+            request.NewPassword = string.Empty;
+
+            var useCase = CreateUseCase(user, request.Password);
+
+            var exception = await Assert.ThrowsAsync<ErrorOnValidationException>(() =>
+                                       useCase.Execute(request));
+            Assert.Contains(ResourceErrorMessages.INVALID_PASSWORD, exception.GetErros());
+
+        }
+
+        [Fact]
+        public async Task Error_CurrentPassword_Different()
+        {
+            var user = UserBuilder.Build();
+            var request = RequestChangePasswordJsonBuilder.Build();         
+            var useCase = CreateUseCase(user);
+            var exception = await Assert.ThrowsAsync<ErrorOnValidationException>(() =>
+                                       useCase.Execute(request));
+            Assert.Contains(ResourceErrorMessages.PASSWORD_DIFFERENT_CURRENT_PASSWORD, exception.GetErros());
+        }
+
+        private static ChangePasswordUseCase CreateUseCase(CashFlow.Domain.Entities.User user, string? password = null)
+        {
+            var unitOfWork = UnitOfWorkBuilder.Build();
+            var userUpdateOnlyRepository = UserUpdateOnlyRepositoryBuilder.Build(user);
+            var loggedUser = LoggedUserBuilder.Build(user);
+            var passwordEncripter = new PasswordEncripterBuilder().Verify(password).Build();
+
+            return new ChangePasswordUseCase(loggedUser,
+                                           userUpdateOnlyRepository,
+                                           unitOfWork,
+                                           passwordEncripter);
+
+        }
+    }
+}
