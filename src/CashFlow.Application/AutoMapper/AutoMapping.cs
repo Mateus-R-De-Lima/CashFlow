0f80be0dffc52cf833d30ab56f8fe4f2ab98a676
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-01-02 20:15:05 -0300

    feat- implementando as tags na criação da despensas.

--- src/CashFlow.Application/AutoMapper/AutoMapping.cs
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CashFlow.Communication.Enums;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
@@ -15,7 +16,11 @@ namespace CashFlow.Application.AutoMapper
 
         private void AutoMappingRequest()
         {
-            CreateMap<RequestExpenseJson, Expense>();
+            CreateMap<RequestExpenseJson, Expense>()
+                .ForMember(dest => dest.Tags, config => config.MapFrom(origem => origem.Tags.Distinct()));
+
+            CreateMap<Communication.Enums.Tag, Domain.Entities.Tag>()
+                .ForMember(dest => dest.Value, config => config.MapFrom(origem => origem));
 
             #region User
             CreateMap<RequestUserJson, User>()
---
+++ src/CashFlow.Communication/Enums/Tag.cs
@@ -0,0 +1,16 @@
+﻿namespace CashFlow.Communication.Enums
+{
+    public enum Tag
+    {
+        Health,
+        Essentials,
+        Variable,
+        Fixed,
+        Personal,
+        Emergency,
+        Investment,
+        Leisure,
+        Education,
+        Transportation
+    }
+}
